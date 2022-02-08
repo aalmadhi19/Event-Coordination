d1a9d807ce0d473e491d5332e37526ba2fb41119
Abdulaziz Almadhi <a.almadhi@mu.edu.sa>
2022-02-08 20:45:51 +0300

    commit message

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use App\Models\Contact;
+use App\Models\Organization;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'azoz',
+            'email' => 'johndoe@example.com',
+            'password' => 'secret',
+        ]);
+
+    }
+}
